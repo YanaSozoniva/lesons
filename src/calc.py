b1569a785154efa50dc486f2fad8afe9d2a2cc87
Yana Sozonova <ynakochneva@mail.ru>
2024-08-07 22:07:23 +0500

    test parametrize

--- src/calc.py
+++ src/calc.py
@@ -19,3 +19,4 @@ def calculate_logarithm(number):
     if number <= 0:
         raise ValueError("Логарифм можно вычислить только для положительных чисел")
     return math.log(number)
+
--- tests/test_code.py
+++ tests/test_code.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.code import up_first, reverse_string
 
 def test_up_first():
@@ -6,9 +8,19 @@ def test_up_first():
 def test_up_first_for_empty():
     assert up_first('') == ''
 
-def test_reverse_string_num(numbers):
-    assert reverse_string("123") == numbers
+# def test_reverse_string_num(numbers):
+#     assert reverse_string("123") == numbers
+#
+#
+# def test_reverse_string_let(letters):
+#     assert reverse_string("hello") == letters
 
-
-def test_reverse_string_let(letters):
-    assert reverse_string("hello") == letters
+@pytest.mark.parametrize(
+    "val, expected",
+    [("hello", "olleh"),
+    ("world", "dlrow"),
+    ("12345", "54321"),
+    ("", ""),
+     ])
+def test_reverse_string(val, expected):
+    assert reverse_string(val) == expected
